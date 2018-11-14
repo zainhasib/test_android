304a8dcacc8ca7d9a2109409e167ced83f88458f
Zain Hasib <zainhasib@gmail.com>
2018-11-14 16:42:54 +0530

    das

--- app/src/main/java/com/example/zain/test/MainActivity.java
+++ app/src/main/java/com/example/zain/test/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.zain.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendMsg, call;
+    EditText number, message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner dropdown = findViewById(R.id.spinner);
-        String[] items = new String[]{"1", "2", "3"};
+        String[] items = new String[]{"9.6", "9.3", "9.0"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
         dropdown.setAdapter(adapter);
+
+        sendMsg = findViewById(R.id.msg);
+        call = findViewById(R.id.call);
+        number = findViewById(R.id.number);
+        message = findViewById(R.id.message);
+
+        sendMsg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mess = message.getText().toString();
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.putExtra("sms_body", mess);
+                sendIntent.setType("vnd.android-dir/mms-sms");
+                startActivity(sendIntent);
+            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "CALL", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
